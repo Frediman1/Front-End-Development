0cbc3af03b3618bb5ba0f413746eb32039ab17d6
Fredrick Evans <Fredrickevans21@gmail.com>
2024-09-12 07:46:54 -0400

    made proper changes and fixed java scripts error

--- forms.js
+++ forms.js
@@ -1,9 +1,43 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    var email = document.getElementById('email').value;
-    var confirmEmail = document.getElementById('confirmEmail').value;
-    
-    if (email !== confirmEmail) {
-        alert('Emails do not match!');
-        event.preventDefault();
+// document.getElementById('contactForm').addEventListener('submit', function (event) {
+//     // var email = document.getElementById('email').value;
+//     // var confirmEmail = document.getElementById('confirmEmail').value;
+
+//     // if (email !== confirmEmail) {
+//     //     alert('Emails do not match!');
+//     //     event.preventDefault();
+//     // }
+
+//     // Get email and confirm email field values
+//     const email = document.getElementById('email').value;
+//     const confirmEmail = document.getElementById('confirmEmail').value;
+
+//     // Check if the email fields match
+//     if (email !== confirmEmail) {
+//         alert("Email addresses do not match. Please check again.");
+//         // Prevent form submission
+//         event.preventDefault();
+
+//         // Show alert if emails do not match
+//         //    alert("Email addresses do not match. Please check again.");
+//     }
+
+// });
+
+const form = document.getElementById('contactForm');
+form.addEventListener('click', checkEmail);
+function checkEmail(event) {
+    const email = document.getElementById('email').value;
+    const confirm = document.getElementById('confirmEmail').value;
+
+    if (email === '' || confirm === '') {
+        return
     }
-});
+
+    if (email === confirm) {
+        document.getElementById('form').onsubmit();
+    }
+    else {
+        event.preventDefault();
+        alert('Emails do not match');
+    }
+}
